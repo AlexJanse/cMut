cf9df65809d744ae0b71ec17c0abf7695902c9de
Janse <Z388215@umcn.nl>
2018-09-24 11:24:16 +0200

    Optimising the shufflemutation function

--- R/shuffleMutations.R
+++ R/shuffleMutations.R
@@ -8,7 +8,6 @@
 #' @param altHeader A string with the column header of the alternative nucleotide.
 #' @param surroundingHeader A string with the column header of the surrounding nucleotides.
 #' @param nBootstrap A number with the ammount of bootstraps there need to be excecuted.
-#' @param showProgress A boolean whether or not to show the progress.
 #' @inheritParams linkPatterns
 #' @inheritParams identifyAndAnnotateClusters
 #' @import magrittr
@@ -21,8 +20,6 @@ shuffleMutations <- function(x,chromHeader = "chrom",
                              surroundingHeader = "surrounding",
                              sampleIdHeader = "sampleIDs",
                              nBootstrap = 1000,
-                             showProgress = TRUE,
-                             linkPatterns = FALSE,
                              maxDistance = 20000,
                              reverseComplement = FALSE,
                              searchPatterns = NULL,
@@ -32,19 +29,34 @@ shuffleMutations <- function(x,chromHeader = "chrom",
                              searchIdHeader = "proces",
                              searchReverseComplement = TRUE,
                              tibble = TRUE){
+  # Get the search table with known mutation patterns -------------------------------
+  if(is.null(searchPatterns)){
+    # Get default table if nothing is sent
+    tempWd <- setwd(paste0(.libPaths(),"/cMut")) # To avoid connection issues with the required files
+    on.exit(setwd(tempWd), add = T) # Set the wd back to the orignal one
+    resultTable <- tibble::as.tibble(readRDS("data/mutationPatterns.rds")) %>%
+      dplyr::mutate_all(as.character)
+  } else {
+    resultTable <- searchPatterns
+  }
 
-  shuffleTable <- createShuffleTable(x,chromHeader,
-                                     positionHeader,
-                                     refHeader,
-                                     altHeader,
-                                     surroundingHeader,
-                                     sampleIdHeader,
-                                     nBootstrap,
-                                     showProgress)
+  x[sapply(x, is.factor)] <- lapply(x[sapply(x, is.factor)],as.character)
 
-  if(linkPatterns == TRUE){
-    # Create a table with the patterns and their frequency ----------------------
-    patternFreq <- createPatternFreq(chromHeader = "chrom",
+  # Add a frequence column to fill up during bootstrapping
+  resultTable <- dplyr::mutate(resultTable, frequency = rep.int(0,nrow(resultTable)))
+
+  # Preform bootstrap ------------------------------------------------------------
+  for(bootstrap in 1:nBootstrap){
+    # Create a table with shuffled mutations and contexts ------------------------
+    shuffleTable <- createShuffleTable(x,chromHeader,
+                                       positionHeader,
+                                       refHeader,
+                                       altHeader,
+                                       surroundingHeader,
+                                       sampleIdHeader)
+
+    # Add the frequencies of patterns to the resultTable -----------------------
+    resultTable <- addToResultTable(chromHeader = "chrom",
                                      positionHeader = "pos",
                                      refHeader = "ref",
                                      altHeader = "alt",
@@ -52,23 +64,18 @@ shuffleMutations <- function(x,chromHeader = "chrom",
                                      sampleIdHeader = "sampleIDs",
                                      maxDistance,
                                      shuffleTable,
-                                     linkPatterns,
                                      reverseComplement,
-                                     searchPatterns,
+                                     searchPatterns = resultTable,
                                      searchRefHeader,
                                      searchAltHeader,
                                      searchContextHeader,
                                      searchIdHeader,
                                      searchReverseComplement)
-
-    total = sum(patternFreq$frequency)
-    results <- dplyr::mutate(patternFreq, percentage = frequency/total*100)
-  } else {
-    # Add the percentage to the shuffleTable ------------------------------------
-    total = sum(shuffleTable$n)
-    results <- dplyr::mutate(shuffleTable,percentage = n/total*100)
   }
 
+  # Calculate the percentage of the frequecies -----------------------------------
+  total = sum(resultTable$frequency)
+  results <- resultTable %>% dplyr::mutate(percentage = purrr::map_dbl(frequency,function(x){x/total*100}))
 
   if(tibble){
     return(tibble::as.tibble(results))
@@ -88,56 +95,48 @@ createShuffleTable <-  function(x,chromHeader,
                                 refHeader,
                                 altHeader,
                                 surroundingHeader,
-                                sampleIdHeader,
-                                nBootstrap,
-                                showProgress){
-  shuffleTable <- data.frame(chom = c(), pos = c(),sampleIDs = c(), stringsAsFactors = F)
+                                sampleIdHeader){
 
-  foreach::foreach(i = 1:nBootstrap) %do% {
-    if(showProgress){
-      svMisc::progress(i/nBootstrap*100)
-    }
-    ref <- dplyr::pull(x,refHeader)
+  # Extract the muation data from the table --------------------------------------
+  ref <- dplyr::pull(x,refHeader)
+  alt <- dplyr::pull(x,altHeader)
+  surrounding <- dplyr::pull(x,surroundingHeader)
 
-    surrounding <- dplyr::pull(x,surroundingHeader)
+  # Create a table with random picked reference and surrounding nucleotides -------
+  shuffleTable <- data.frame(ref = sample(ref,length(ref)),stringsAsFactors = F) %>%
+    dplyr::mutate(surrounding = sample(surrounding,length(surrounding)))
 
-    tempShuffleTable <- data.frame(ref = sample(ref,length(ref)),stringsAsFactors = F) %>%
-      dplyr::mutate(surrounding = sample(surrounding,length(surrounding)))
+  again <- TRUE
+  while(again){
+    again <- FALSE
 
-    again <- TRUE
-    while(again){
-      again <- FALSE
-      shuffleAlt <- c()
-      alt <- dplyr::pull(x,altHeader)
+    shuffleAlt <- c()
+    alt <- dplyr::pull(x,altHeader)
 
-      foreach::foreach(index = 1:length(ref)) %do% {
-        grabbag <- alt
-        random <- sample(grabbag,1)[[1]]
-        while(length(grabbag) != 0 & random == tempShuffleTable[index,1][[1]]){
-          grabbag <- grabbag[-match(random,grabbag)]
-          if(length(grabbag) > 0){
-            random <- sample(grabbag,1)[[1]]
-          }
-        }
+    # A loop to chosse a random alternative nucleotide while making sure that it's not the same as the reference ---------
+    foreach::foreach(index = 1:length(ref)) %do% {
+      grabbag <- alt
 
-        if(length(grabbag) == 0 & random == tempShuffleTable[index,1][[1]]){
-          again = TRUE
-        }
-
-        alt <- alt[-match(random,alt)]
-        shuffleAlt[length(shuffleAlt)+1] <- random
+      refNuc = shuffleTable[index,1][[1]]
+      grabbag <- setdiff(grabbag,c(refNuc))
+      if(length(grabbag) == 0){
+        again = TRUE
+      } else{
+        random <- as.character(sample(grabbag,1)[[1]])
       }
+
+      alt <- alt[-match(random,alt)]
+      shuffleAlt[length(shuffleAlt)+1] <- random
     }
-
-    tempShuffleTable <- tempShuffleTable %>%
-      dplyr::mutate(sampleIDs = dplyr::pull(x,sampleIdHeader),
-                    chrom = dplyr::pull(x,chromHeader),
-                    pos = dplyr::pull(x,positionHeader),
-                    alt = shuffleAlt,
-                    check = !alt == ref)
-
-    shuffleTable <- dplyr::bind_rows(shuffleTable,tempShuffleTable)
   }
+
+  shuffleTable <- shuffleTable %>%
+    dplyr::mutate(sampleIDs = dplyr::pull(x,sampleIdHeader),
+                  chrom = dplyr::pull(x,chromHeader),
+                  pos = dplyr::pull(x,positionHeader),
+                  alt = shuffleAlt,
+                  check = !alt == ref)
+
   nTable <- dplyr::count(dplyr::group_by(shuffleTable[c("sampleIDs","chrom","pos","ref","alt","surrounding","check")],
                                          sampleIDs,
                                          chrom,
@@ -149,19 +148,16 @@ createShuffleTable <-  function(x,chromHeader,
 
   tempDF <- data.frame(stringsAsFactors = f)
   shuffleTable <- dplyr::bind_rows(tempDF,nTable) # Needed to avoid a grouped table which will lead to errors further down the path
-  if(showProgress){
-    print("Bootstrap done!")
-  }
 
   return(shuffleTable)
 }
 
-#' createPatternFreq
+#' addToResultTable
 #' @description A function to create a table with frequencies of patterns.
 #' @inheritParams shuffleMutations
 #' @import magrittr
 #' @import foreach
-createPatternFreq <- function(chromHeader,
+addToResultTable <- function(chromHeader,
                               positionHeader,
                               refHeader,
                               altHeader,
@@ -169,7 +165,6 @@ createPatternFreq <- function(chromHeader,
                               sampleIdHeader,
                               maxDistance,
                               shuffleTable,
-                              linkPatterns,
                               reverseComplement,
                               searchPatterns,
                               searchRefHeader,
@@ -180,25 +175,14 @@ createPatternFreq <- function(chromHeader,
 
   # Add patterns that match with the random mutations in the shuffleTable ----------------
   clusterTablePerMut <- identifyAndAnnotateClusters(x = shuffleTable,maxDistance = maxDistance,positionHeader = "pos",linkPatterns = T)
-  print("ident")
   clusterTable <- groupClusters(clusterTablePerMut,patternIntersect = T)
-  print("clusterd")
-  if(is.null(searchPatterns)){
-    # Get default table if nothing is sent ----------------------------------------
-    tempWd <- setwd(paste0(.libPaths(),"/cMut")) # To avoid connection issues with the required files
-    on.exit(setwd(tempWd), add = T) # Set the wd back to the orignal one
-    searchPatterns <- tibble::as.tibble(readRDS("data/mutationPatterns.rds")) %>%
-      dplyr::mutate_all(as.character)
-  }
 
-  searchPatterns$frequency <- rep.int(0,nrow(searchPatterns)) # Add a column to the pattern table for the frequencies
-
-  foreach::foreach(index = 1:nrow(clusterTable)) %do% {
+  for(index in 1:nrow(clusterTable)){
     if(clusterTable[index,"has.intersect"] == TRUE){
-      foreach::foreach(patroon = clusterTable[index,"patternIntersect"][[1]][[1]]) %do% {
-        frequency <- searchPatterns[grep(searchIdHeader, names(searchPatterns)) == patroon,"frequency"]
+      for(patroon in clusterTable[index,"patternIntersect"][[1]][[1]]){
+        frequency <- searchPatterns[searchPatterns[,grep(searchIdHeader, names(searchPatterns))] == patroon,"frequency"][[1]]
         addFreq <- sum(clusterTable[index,"cMuts"][[1]][[1]][,"n"])
-        searchPatterns[grep(searchIdHeader, names(searchPatterns)) == patroon,"frequency"] <- frequency + addFreq
+        searchPatterns[searchPatterns[,grep(searchIdHeader, names(searchPatterns))] == patroon,"frequency"] <- frequency + addFreq
       }
     }
   }
